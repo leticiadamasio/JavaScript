8312e78844563fd4fb3d06a1eedb4c96fab53ea7
leticiadamasio <damasioleticia03@gmail.com>
2022-01-12 16:53:36 -0300

    Testando JavaScript

---
+++ sript.js
@@ -0,0 +1,23 @@
+// Crie uma variável que receba um nome;
+let nome = "Letícia"
+let name = 'Letícia'
+let templatstring = `Letícia`
+
+// Crie uma variável que receba um número;
+let number = 18
+
+// Crie uma variável com o ano atual e subtraia pelo número que você guardou;
+let year = 2022 - number
+
+// Agora mostre no console o resultado da soma das duas variáveis guardadas anteriormente;
+let soma = year + number
+console.log (soma)
+
+//Mostre no console a mensagem "Estou mexendo no console.log";
+console.log ("Estou mexendo no console.log")
+
+// Crie uma variável chamada “quartaFeira” e atribua à ela o valor booleano que representa verdadeiro ou falso e mostre a saída no console;
+let quartaFeira = true
+
+// Exiba no console o tipo de dado da váriavel quartaFeira;
+console.log (quartaFeira)
